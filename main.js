6a3180c7932349ec5c264c9aaf286d4e1ae535fa
Sara Nofal <sarah.nehad91@gmail.com>
2022-12-04 14:39:12 +0200

    update

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ const closeSideNav = () => mainNavigation.classList.remove("active");
 document.addEventListener("swiped-left", closeSideNav);*/
 toggler.addEventListener("click", openSideNav);
 overlay.addEventListener("click", closeSideNav);
+
+var carouselWidth = $('.carousel-inner')[0].scrolWidth;
+var cardWidth = $('.carousel-item').width();
+
+var scrolPosition = 0;
+$('.carousel-control-next').on('click', function () {
+    // if (scrolPosition < (carouselWidth - (cardWidth * 3))) {
+    scrolPosition = scrolPosition + cardWidth;
+    $('.carousel-inner').animate({ scrollLeft: scrolPosition }, 600);
+    //}
+})
+$('.carousel-control-prev').on('click', function () {
+    if (scrolPosition > 0) {
+        scrolPosition = scrolPosition - cardWidth;
+        $('.carousel-inner').animate({ scrollLeft: scrolPosition }, 600);
+    }
+})
